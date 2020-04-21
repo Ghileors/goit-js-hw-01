0b23e6e8c05fa88593ed9f0a7af81afb69fdef8f
Polina <ghileors@gmail.com>
2020-04-21 10:30:54 +0300

    Bug in exercise 4 fixed.

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -1,11 +1,9 @@
 const pricePerDroid = 300;
 let credits = 23580;
 
-const answ = prompt('Количество дроидов, которое вы хотите приобрести:');
+let answ = prompt('Количество дроидов, которое вы хотите приобрести:');
 
-answ = Number(answ);
-
-let totalPrice = pricePerDroid * answ;
+let totalPrice = pricePerDroid * Number(answ);
 
 if (answ === null) {
   alert('Отменено пользователем!');
